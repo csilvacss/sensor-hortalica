18251bd20c9d1e81d059c8e07336cf3968e2f39f
Carlos Silva <carlos.silva89@outlook.com>
2022-12-23 16:33:27 -0300

    primeiro envio

---
+++ Estufa_hortalicas.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+int PinVer = 6;
+int PinAzu = 7;
+int PinMot = 8;
+int PinBuz = 9;
+
+float valorTPM = 0;
+float tensao = 0;
+float temp = 0;
+
+void setup()
+{
+    pinMode(PinAzu, OUTPUT);
+    pinMode(PinBuz, OUTPUT);
+    pinMode(PinVer, OUTPUT);
+    pinMode(PinMot, OUTPUT);
+    pinMode(A0, INPUT);
+
+    Serial.begin(9600);
+
+    digitalWrite(PinAzu,HIGH);
+    digitalWrite(PinBuz,LOW);
+    digitalWrite(PinVer,LOW);
+    digitalWrite(PinMot,LOW);
+}
+
+void loop()
+{
+    valorTPM = analogRead(A0);
+    tensao = valorTPM*(5000/1024);	//Converte a leitura A0 em tensão
+    temp = (tensao-500)/10;			// Converte a tensão na temperatura
+
+    Serial.print(" Temperatura: ");
+    Serial.print(valorTPM);
+    Serial.print(" \xB0"); 	// mostra o símbolo de grau
+    Serial.println("C"); 		// mostra a unidade de tempetatura
+    
+    if(temp <= 30)
+    {
+        digitalWrite(PinAzu,HIGH);
+        digitalWrite(PinBuz,LOW);
+        digitalWrite(PinVer,LOW);
+        digitalWrite(PinMot,LOW);
+    }
+    if(temp > 30 and temp <= 50)
+    {
+        digitalWrite(PinAzu,LOW);
+        digitalWrite(PinBuz,LOW);
+        digitalWrite(PinVer,LOW);
+        digitalWrite(PinMot,HIGH);
+
+    }
+    if(temp > 50)
+    {
+        digitalWrite(PinAzu,LOW);
+        digitalWrite(PinBuz,HIGH);
+        digitalWrite(PinVer,HIGH);
+        digitalWrite(PinMot,HIGH);
+    }
+    delay(2000);
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+// Esse projeto é uma aplicação de estufa de hortaliça
+
+/* 
+O código foi desenvolvido em C++
+foi feito em simulação virtual no site "https://www.tinkercad.com/"
+
+O projeto segue a seguinte funcões e processos:
+
+(1) Faz a leitura da temperatura através do sensor, e aciona o LED azul enquanto em funcionameto normal;
+
+(2) Aciona o motor de ventilador caso a temperatura seja igual ou maior a 30 °C;
+
+(3) Caso a temperatura ultrapasse os 50 °C, o LED azul apaga, o LED vermelho acende e a buzina aciona, avisando uma situação de emergência.
+*/
